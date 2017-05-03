33fdd4aa162ee39d533aac6eb03d5121fedb5d40
githubmaster1987 <silverstart1987913@yahoo.com>
2017-05-03 14:55:24 -0400

    Update Code & Selenium Completed

--- phantom.py
+++ phantom.py
@@ -30,7 +30,6 @@ from time import sleep
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver import ActionChains
 
-
 s = Scraper(
 	use_cache=False, #enable cache globally
 	retries=2,
@@ -39,6 +38,12 @@ s = Scraper(
 	proxy_file = 'proxy.txt',
 	proxy_auth= 'silicons:1pRnQcg87F'
 	)
+luminati_zone_proxy_username = "lum-customer-hl_1dafde3b-zone-zone1"
+luminati_zone_proxy_pwd = "n9ndhce734x9"
+
+luminati_proxy_host = "zproxy.luminati.io"
+luminati_proxy_port = 22225
+
 
 logger = s.logger
 
@@ -54,6 +59,11 @@ DRIVER_WAITING_SECONDS = 60
 DRIVER_MEDIUM_WAITING_SECONDS = 10
 DRIVER_SHORT_WAITING_SECONDS = 3
 
+MOBILE_SC = [[480,800], [240,320], [800,480], [800,1280], [720,1280], [1920,1080], [2560,1440], [1440,2560], [1280,720], [960,540], [480,360], [1334,750], [640,1136], [960,640], [480,320]]
+TABLET_SC = [[1280,800], [1024,600], [1024,768], [2048,1536], [2732,2048], [768,1024], [768,1280]]
+DESKTOP_SC = [[1920,1080], [1600,900], [1280,1024], [1152,864], [1024,768], [800,600]]
+
+
 class AnyEc:
 	""" Use with WebDriverWait to combine expected_conditions
 		in an OR.
@@ -67,6 +77,64 @@ class AnyEc:
 			except:
 				pass
 
+def create_phantomjs_driver(c_type='http', ua=None, proxy=None, screen_resolution=None):
+    if ua == None:
+        ua = ["D", "W", "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:53.0) Gecko/20100101 Firefox/53.0"]
+        # e.g. ["M", "A", "Mozilla/5.0 (Android; Mobile; rv:40.0) Gecko/40.0 Firefox/40.0"],
+        if ua[0] == "M": # Mobile
+            screen_resolution = random.choice(MOBILE_SC)
+        elif ua[0] == "T": # Tablet
+            screen_resolution = random.choice(TABLET_SC)
+        else: # elif ua[0] == "D": # Desktop
+            screen_resolution = random.choice(DESKTOP_SC)
+
+    proxy_ip = random_luminati_proxy()
+    #proxy_ip = "173.245.85.243:29842"
+
+    dcap = dict(DesiredCapabilities.PHANTOMJS)
+    #dcap["phantomjs.page.settings.userAgent"] = ua[2]
+    dcap['phantomjs.page.settings.loadImages'] = True
+    dcap['phantomjs.page.settings.resourceTimeout'] = 60000
+
+    #dcap['phantomjs.page.customHeaders.User-Agent'] = ua[2]
+#    dcap['phantomjs.page.customHeaders.Accept'] = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"
+    #dcap['phantomjs.page.customHeaders.Accept-Encoding'] = "gzip, deflate, sdch, br"
+    
+    accept_language = "en-US,en;q=0.8"
+   
+    if accept_language != "":
+       dcap['phantomjs.page.customHeaders.Accept-Language'] = accept_language
+    
+    # Proxy Provider proxy
+    # auth_str = "%s:%s" % ("lether", "C6zG2Dyh")
+    # service_args = ['--proxy=%s' % proxy_ip, '--proxy-type=%s' % c_type,'--proxy-auth=%s' % auth_str , '--ignore-ssl-errors=true', '--ssl-protocol=any'] 
+
+    #Luminati Proxy Service
+    proxy_str = "{}:{}".format(luminati_proxy_host, luminati_proxy_port)
+
+    auth_str = "{}-ip-{}:{}".format(luminati_zone_proxy_username, proxy_ip, luminati_zone_proxy_pwd)
+    service_args = ['--proxy=%s' % proxy_str, '--proxy-type=%s' % c_type,'--proxy-auth=%s' % auth_str , '--ignore-ssl-errors=true', '--ssl-protocol=any'] 
+
+    # print('user-agent -> ' + ua[2])
+    # print('proxy -> %s %s, language -> %s' % (proxy_ip, auth_str, accept_language))
+    # print('screen resolution -> %d x %d' % (screen_resolution[0], screen_resolution[1]))
+    # print (service_args)
+    try:
+        driver = webdriver.PhantomJS(desired_capabilities=dcap, service_args=service_args)
+        #driver = webdriver.PhantomJS()
+    except WebDriverException as e:
+        print("webdriver.PhantomJS WebDriverException -> %s" % str(e))
+        try:
+            phantom_Quit(driver)
+        except:
+            pass
+        return None, ua, proxy, screen_resolution
+
+    driver.set_window_size(screen_resolution[0], screen_resolution[1])
+    driver.implicitly_wait(DRIVER_WAITING_SECONDS)
+    driver.set_page_load_timeout(DRIVER_WAITING_SECONDS)
+
+    return driver, ua, proxy_ip, screen_resolution
 
 def get_geolocation():
 	with open(city_file) as csvfile:
@@ -259,13 +327,14 @@ def start_selenium():
 	luminati_proxy_port = 22225
 
 	for ind, url in enumerate(url_lists):
-		if ind<200 or ind>300:
+		if ind < 50:
 			continue
+
 		logger.info("Index-------------------- -> {}".format(ind))
 
-		proxy_ip = random_luminati_proxy()
-		proxy_str = "{}:{}".format(luminati_proxy_host, luminati_proxy_port)
-		auth_str = "{}-ip-{}".format(luminati_zone_proxy_username, proxy_ip, )
+		# proxy_ip = random_luminati_proxy()
+		# proxy_str = "{}:{}".format(luminati_proxy_host, luminati_proxy_port)
+		# auth_str = "{}-ip-{}".format(luminati_zone_proxy_username, proxy_ip, )
 		
 		# proxyauth_plugin_path = create_proxyauth_extension(
 		# 		proxy_host=luminati_proxy_host,
@@ -273,60 +342,67 @@ def start_selenium():
 		# 		proxy_username=auth_str,
 		# 		proxy_password=luminati_zone_proxy_pwd
 		# 	)
+				
+		# co = Options()
+		# co.add_argument("--start-maximized")
+		# co.add_extension(proxyauth_plugin_path)
+		# driver = webdriver.Chrome(chrome_options=co)
 
-		co = Options()
-		co.add_argument("--start-maximized")
-		#co.add_extension(proxyauth_plugin_path)
-		driver = webdriver.Chrome(chrome_options=co)
+		driver, user_agent, proxy, screen_resolution = create_phantomjs_driver()       #PHANTOMJS PART
 
 		sleep(random.randrange(DRIVER_MEDIUM_WAITING_SECONDS))
+
 		driver.get(url)
 		logger.info(url)
 		while(1):
-			try:
-				sleep(random.randrange(DRIVER_MEDIUM_WAITING_SECONDS))
-				logger.info("Wait page loading")
-				pagination_container = WebDriverWait(driver, DRIVER_WAITING_SECONDS).until(AnyEc
-					(
-						EC.presence_of_element_located(
-							(By.XPATH, "//div[@id='box1 d-pad-30']")
-						),
-						EC.presence_of_element_located(
-							(By.XPATH, "//div[@class='unable-to-find']")
-						),
-					)
+			# try:
+			sleep(random.randrange(DRIVER_MEDIUM_WAITING_SECONDS))
+			logger.info("Wait page loading")
+			driver.save_screenshot("a.png")
+			pagination_container = WebDriverWait(driver, DRIVER_WAITING_SECONDS).until(AnyEc
+				(
+					EC.presence_of_element_located(
+						(By.XPATH, "//div[@id='box1 d-pad-30']")
+					),
+					EC.presence_of_element_located(
+						(By.XPATH, "//div[@class='unable-to-find']")
+					),
 				)
+			)
 
-				logger.info("Page is loaded")
+			logger.info("Page is loaded")
 
-				try:
-					unabel_find = driver.find_element_by_xpath("//div[@class='unable-to-find']")
-				except Exception as e:
-					logger.info(e)
+			try:
+				unabel_find = driver.find_element_by_xpath("//div[@class='unable-to-find']")
+			except Exception as e:
+				logger.info(e)
 
-				doc = Doc(html=driver.page_source)
-				divs = doc.q("//li[@class='box1 d-pad-30']")
-				logger.info(len(divs))
-				for div in divs:
-					href = div.x("h2/a/@href").strip()
-					item = [
-						"url", href,
-						"parent", url.replace("&page=1", ""),
-						"ind", ind
-					]
-					s.save(item, list_file)
-					logger.info(href)
+			doc = Doc(html=driver.page_source)
+			divs = doc.q("//li[@class='box1 d-pad-30']")
+			logger.info(len(divs))
+			for div in divs:
+				href = div.x("h2/a/@href").strip()
+				item = [
+					"url", href,
+					"parent", url.replace("&page=1", ""),
+					"ind", ind
+				]
+				s.save(item, list_file)
+				logger.info(href)
 
-				logger.info ("Next Button")
-				next_button = driver.find_element_by_xpath('//a[@class="next_page"]')
-				driver.execute_script('window.scrollTo(0, ' + str(next_button.location['y']) + ');')
-				next_button.click()
-				sleep(random.randrange(DRIVER_MEDIUM_WAITING_SECONDS))
-			except:
-				break
+			logger.info ("Next Button")
+			driver.save_screenshot("b.jpg")
+			next_button = driver.find_element_by_xpath('//a[contains(@class,"next_page")]')
+			driver.execute_script('window.scrollTo(' + str(next_button.location['x']) + ', ' + str(next_button.location['y']) + ');')
+			
+			sleep(random.randrange(DRIVER_SHORT_WAITING_SECONDS))
+			next_button.click()
+			sleep(random.randrange(DRIVER_MEDIUM_WAITING_SECONDS))
+			# except:
+			# 	break
 		
 		driver.quit()
-
+		break
 		
 if __name__ == '__main__':
 	
